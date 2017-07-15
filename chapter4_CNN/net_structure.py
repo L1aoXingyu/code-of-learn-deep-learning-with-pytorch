f02341736fa3dfe213d227141f4e266d4322410c
Sherlock <sherlockliao01@gmail.com>
2017-07-15 21:50:47 +0800

    update

---
+++ chapter4_CNN/net_structure.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()  # b, 3, 32, 32
+        layer1 = nn.Sequential()
+        layer1.add_module('conv1', nn.Conv2d(3, 32, 3, 1, padding=1))
+        # b, 32, 32, 32
+        layer1.add_module('relu1', nn.ReLU(True))
+        layer1.add_module('pool1', nn.MaxPool2d(2, 2))  # b, 32, 16, 16
+        self.layer1 = layer1
+
+        layer2 = nn.Sequential()
+        layer2.add_module('conv2', nn.Conv2d(32, 64, 3, 1, padding=1))
+        # b, 64, 16, 16
+        layer2.add_module('relu2', nn.ReLU(True))
+        layer2.add_module('pool2', nn.MaxPool2d(2, 2))  # b, 64, 8, 8
+        self.layer2 = layer2
+
+        layer3 = nn.Sequential()
+        layer3.add_module('conv3', nn.Conv2d(64, 128, 3, 1, padding=1))
+        #b, 128, 8, 8
+        layer3.add_module('relu3', nn.ReLU(True))
+        layer3.add_module('pool3', nn.MaxPool2d(2, 2))  #b, 128, 4, 4
+        self.layer3 = layer3
+
+        layer4 = nn.Sequential()
+        layer4.add_module('fc1', nn.Linear(2048, 512))
+        layer4.add_module('fc_relu1', nn.ReLU(True))
+        layer4.add_module('fc2', nn.Linear(512, 64))
+        layer4.add_module('fc_relu2', nn.ReLU(True))
+        layer4.add_module('fc3', nn.Linear(64, 10))
+        self.layer4 = layer4
+
+    def forward(self, x):
+        conv1 = self.layer1(x)
+        conv2 = self.layer2(conv1)
+        conv3 = self.layer3(conv2)
+        fc_input = conv3.view(conv3.size(0), -1)
+        fc_out = self.layer4(fc_input)
+        return fc_out
+
+
+model = SimpleCNN()
+
+new_model = nn.Sequential(*list(model.children())[:2])
+print(new_model)
